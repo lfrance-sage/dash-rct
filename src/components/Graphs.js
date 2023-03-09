2e7f03b705f36eed19a0be203a49275f3500ecc3
Luka France <luka.france@sage.com>
2023-03-09 16:03:08 +0000

    V2.4

--- src/components/Graphs.js
+++ src/components/Graphs.js
@@ -15,7 +15,7 @@ const Graphs = () => {
         <GlobalStyle />
      
         <NavigationBar navigationType="black">Sage.com Dashboard</NavigationBar>
-        <h1>End User Metrics:</h1>
+  
        
   <div className="row1">
     <iframe
@@ -55,46 +55,38 @@ const Graphs = () => {
       src="https://chart-embed.service.newrelic.com/herald/3af70ae3-567f-477c-b5e8-9efa5faf7e21?height=250px&timepicker=true&width=350px"
     ></iframe>
   </div>
-  <h1>Sage.com Metrics:</h1>
+
   <div className="row2">
     <iframe
       className="iframe1"
       scrolling="no" 
       frameBorder=""
-      src="https://chart-embed.service.newrelic.com/herald/e64c6100-17a6-45f5-ba50-e24e6b7a5f99?height=300px&timepicker=true&width=650px"
+      src="https://chart-embed.service.newrelic.com/herald/e64c6100-17a6-45f5-ba50-e24e6b7a5f99?height=300px&timepicker=true&width=375px"
     ></iframe>
         <iframe
       className="iframe1"
       scrolling="no" 
       frameBorder=""
-      src="https://chart-embed.service.newrelic.com/herald/33b62a24-2905-4b21-b850-9c9faf4cdb0f?height=300px&timepicker=truewidth=650px"
+      src="https://chart-embed.service.newrelic.com/herald/33b62a24-2905-4b21-b850-9c9faf4cdb0f?height=300px&timepicker=truewidth=375px"
     ></iframe>
     <iframe
       className="iframe2"
       scrolling="no"
       frameBorder="0"
-      src="https://chart-embed.service.newrelic.com/herald/c5e988ce-20c9-4b2d-b662-06cc5eceef56?height=300px&timepicker=true"
+      src="https://chart-embed.service.newrelic.com/herald/c5e988ce-20c9-4b2d-b662-06cc5eceef56?height=300px&timepicker=true&width=375px"
     ></iframe>
-  </div>
-  
-  <div className="row3">
-    <iframe
+       <iframe
       className="iframe1"
       scrolling="no" 
       frameBorder=""
-      src="https://chart-embed.service.newrelic.com/herald/e64c6100-17a6-45f5-ba50-e24e6b7a5f99?height=400px&timepicker=true&width=650px"
+      src="https://chart-embed.service.newrelic.com/herald/c946e759-d95a-4c28-bdb2-1cd67ae88666?height=250px&timepicker=true&width=320px"
+      
     ></iframe>
-        <iframe
+       <iframe
       className="iframe1"
       scrolling="no" 
       frameBorder=""
-      src="https://chart-embed.service.newrelic.com/herald/e64c6100-17a6-45f5-ba50-e24e6b7a5f99?height=400px&timepicker=true&width=650px"
-    ></iframe>
-    <iframe
-      className="iframe2"
-      scrolling="no"
-      frameBorder="0"
-      src="https://chart-embed.service.newrelic.com/herald/c5e988ce-20c9-4b2d-b662-06cc5eceef56?height=400px&timepicker=true"
+      src="https://chart-embed.service.newrelic.com/herald/450335c4-6686-44eb-9849-67c627dc5a6b?height=300px&timepicker=true&width=375px"
     ></iframe>
   </div>
   <div className="row3">
@@ -102,23 +94,69 @@ const Graphs = () => {
       className="iframe1"
       scrolling="no" 
       frameBorder=""
-      src="https://chart-embed.service.newrelic.com/herald/e64c6100-17a6-45f5-ba50-e24e6b7a5f99?height=400px&timepicker=true&width=650px"
+      src="https://chart-embed.service.newrelic.com/herald/bf8ac494-a09b-4b6d-a05c-a3416fd995d4?height=300px&timepicker=true&width=350px"
     ></iframe>
         <iframe
       className="iframe1"
       scrolling="no" 
       frameBorder=""
-      src="https://chart-embed.service.newrelic.com/herald/e64c6100-17a6-45f5-ba50-e24e6b7a5f99?height=400px&timepicker=true&width=650px"
+      src="https://chart-embed.service.newrelic.com/herald/e1e44d11-5eca-4b34-ac03-80fac7248575?height=300px&timepicker=true&width=320px"
+    ></iframe>
+   
+    <iframe
+      className="iframe2"
+      scrolling="no"
+      frameBorder="0"
+      src=" https://chart-embed.service.newrelic.com/herald/b29c9ae6-2092-4c68-ae71-7b9111fef430?height=300px&timepicker=true&width=350px"
+    ></iframe>
+       <iframe
+      className="iframe1"
+      scrolling="no" 
+      frameBorder=""
+      src="https://chart-embed.service.newrelic.com/herald/020c14a0-d04f-480a-9a8d-0eeeec94dc6c?height=300px&timepicker=true&width=370px"
+    ></iframe>
+       <iframe
+      className="iframe1"
+      scrolling="no" 
+      frameBorder=""
+      src="  https://chart-embed.service.newrelic.com/herald/77d975ef-62e3-4d6b-b61a-60171454d825?height=400px&timepicker=true&width=325px"
+    
+    ></iframe>
+  </div>
+
+  <div className="row4">
+    <iframe
+      className="iframe1"
+      scrolling="no" 
+      frameBorder=""
+      src="https://chart-embed.service.newrelic.com/herald/b8ca46eb-d5f6-419d-836d-e6ae7d94c687?height=300px&timepicker=true&width=350px"
+    ></iframe>
+        <iframe
+      className="iframe1"
+      scrolling="no" 
+      frameBorder=""
+      src="https://chart-embed.service.newrelic.com/herald/9512c769-13da-4c37-9ac0-97286ec459e7?height=300px&timepicker=true&width=320px"
     ></iframe>
     <iframe
       className="iframe2"
       scrolling="no"
       frameBorder="0"
-      src="https://chart-embed.service.newrelic.com/herald/c5e988ce-20c9-4b2d-b662-06cc5eceef56?height=400px&timepicker=true"
+      src="https://chart-embed.service.newrelic.com/herald/4e388330-dba6-4924-9c86-fb04cd411a67?height=300px&timepicker=true&width=375px"
+    ></iframe>
+       <iframe
+      className="iframe1"
+      scrolling="no" 
+      frameBorder=""
+      src="https://chart-embed.service.newrelic.com/herald/aad01706-3cea-4853-853c-0ac21ddd7db7?height=400px&timepicker=true&width=375px"
+    ></iframe>
+       <iframe
+      className="iframe1"
+      scrolling="no" 
+      frameBorder=""
+      src="https://chart-embed.service.newrelic.com/herald/da5b781f-e51f-42a9-b1b7-c1c354579f14?height=300px&timepicker=true&width=350px"
     ></iframe>
   </div>
-  
-  
+
 
      </CarbonProvider> 
     )
